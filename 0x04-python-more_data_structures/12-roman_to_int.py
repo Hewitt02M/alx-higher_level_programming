4708a24971e94bd8915103022b67493d080256bc
Hewitt02M <masakurehewitt21@gmail.com>
2023-12-06 08:56:40 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
-
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
+    if roman_string is None:
+        return 0
+    if type(roman_string) is not str:
+        return 0
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    number = len(roman_string)
+    value_int = romans[roman_string[number-1]]
+    for i in range(number-1, 0, -1):
+        current = romans[roman_string[i]]
+        prev = romans[roman_string[i-1]]
+        if prev >= current:
+            value_int += prev
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            value_int -= prev
+    return value_int
